e0f2ae735df92a993a03f3d275c9bd1b63cf0093
Oguzhan <oguzz.ylcn@gmail.com>
2022-01-15 15:32:45 +0300

    init commit

---
+++ RocketLaunchDemo/RocketLaunchDemo/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+// AppDelegate.swift
+// RocketLaunchDemo
+//
+// Created on 15.01.2022.
+// Copyright (c)  APPWOX. All rights reserved.
+//
+//
+//
+
+
+import UIKit
+
+let appMode = "test" // test or live
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window:UIWindow?
+    var theStoryboard:UIStoryboard!
+    var rootVC:RootViewController!
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        theStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        openRoot()
+        
+        return true
+    }
+
+    func openRoot() {
+        self.rootVC = self.theStoryboard.instantiateViewController(withIdentifier: "RootVC") as? RootViewController
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = self.rootVC
+        self.window?.makeKeyAndVisible()
+    }
+}
+
---
+++ RocketLaunchDemo/RocketLaunchDemo/RootViewController.swift
@@ -0,0 +1,23 @@
+//
+// RootViewController.swift
+// RocketLaunchDemo
+//
+// Created on 15.01.2022.
+// Copyright (c)  APPWOX. All rights reserved.
+//
+//
+//
+
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
